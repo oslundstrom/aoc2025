65eec669d3a0d561404a0e8c1a9e3c6a910cfc2c
Oskar Lundstrom <oslundstrom@gmail.com>
2025-12-02 19:26:38 +0100

    Prettyfied day 2 printout

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,14 +1,20 @@
+use colored::Colorize;
 use std::io;
 use std::io::Read;
 
-fn get_divisor(n_digits: u32, subsize: u32) -> u64 {
+fn get_divisor(num: u64, n_digits: u32, subsize: u32) -> u64 {
     // sum((10**((m-subsize)*k/(nsubs-1)) for k in range(0,nsubs)))
     let mut sum: u64 = 0;
     let n_subs = n_digits / subsize;
     for k in 0..n_subs {
         sum += 10_u64.pow((n_digits - subsize) * k / (n_subs - 1));
     }
-    println!("DIVISOR {sum} (m={n_digits}, i={n_subs})");
+    print!("\rNUM    {num} {: <1$} ", "", 20 - n_digits as usize,);
+    print!(
+        "DIVISOR {sum} {: <1$} (m={n_digits}, i={n_subs}, s={subsize})                           ",
+        "",
+        (10 - n_digits + subsize) as usize
+    );
 
     sum
 }
@@ -18,9 +24,8 @@ fn is_valid(num: u64) -> bool {
     // let num_str = num.to_string();
     (1..(n_digits / 2 + 1)).any(|subsize| {
         if n_digits.is_multiple_of(subsize) {
-            num.is_multiple_of(get_divisor(n_digits, subsize))
+            num.is_multiple_of(get_divisor(num, n_digits, subsize))
         } else {
-            println!("{num} lenght {n_digits} is not multiple of {subsize}");
             false
         }
     })
@@ -36,20 +41,31 @@ fn main() {
     let mut count: u64 = 0;
 
     for entry in contents.split(",") {
-        println!("{entry}");
+        // print!("{entry}");
         if let Some((start, end)) = entry.trim().split_once("-") {
             let start: u64 = start.parse().expect("Could not parse start");
             let end: u64 = end.parse().expect("Could not parse end");
             for num in start..=end {
-                println!("number {num}");
                 if is_valid(num) {
                     count += num;
-                    println!("VALID {num}")
+                    print!("\r");
+                    println!("{}  {}", "VALID".green(), num.to_string().bright_green());
+                    println!(
+                        "{} {}\n",
+                        "RESULT".bright_blue(),
+                        count.to_string().bright_blue()
+                    );
+                } else {
+                    print!("\r");
                 }
             }
         } else {
             continue;
         }
     }
-    println!("RESULT: {count}")
+    println!(
+        "\r{} {}                                         ",
+        "RESULT:".bright_red(),
+        count.to_string().bright_red()
+    )
 }

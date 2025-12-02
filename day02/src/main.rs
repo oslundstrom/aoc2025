df6118f4957f9c463789cca9648388ed3393cb30
Oskar Lundstrom <oslundstrom@gmail.com>
2025-12-02 18:55:29 +0100

    Sucessful working 10101 style matching

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,10 +1,32 @@
-use regex::Regex;
 use std::io;
 use std::io::Read;
 
-fn main() {
-    let re = Regex::new(r"^(\d+)+$").unwrap();
+fn get_divisor(n_digits: u32, subsize: u32) -> u64 {
+    // sum((10**((m-subsize)*k/(nsubs-1)) for k in range(0,nsubs)))
+    let mut sum: u64 = 0;
+    let n_subs = n_digits / subsize;
+    for k in 0..n_subs {
+        sum += 10_u64.pow((n_digits - subsize) * k / (n_subs - 1));
+    }
+    println!("DIVISOR {sum} (m={n_digits}, i={n_subs})");
 
+    sum
+}
+
+fn is_valid(num: u64) -> bool {
+    let n_digits = num.ilog10() + 1;
+    // let num_str = num.to_string();
+    (1..(n_digits / 2 + 1)).any(|subsize| {
+        if n_digits.is_multiple_of(subsize) {
+            num.is_multiple_of(get_divisor(n_digits, subsize))
+        } else {
+            println!("{num} lenght {n_digits} is not multiple of {subsize}");
+            false
+        }
+    })
+}
+
+fn main() {
     let mut stdin = io::stdin();
     let mut contents = String::new();
     stdin
@@ -19,13 +41,10 @@ fn main() {
             let start: u64 = start.parse().expect("Could not parse start");
             let end: u64 = end.parse().expect("Could not parse end");
             for num in start..=end {
-                let n_digits = num.ilog10() + 1;
-                let num_str = num.to_string();
-                if let Some(caps) = re.captures(&num_str) {
+                println!("number {num}");
+                if is_valid(num) {
                     count += num;
-                    println!("MATCH {num} [tally: {count}] {}", &caps[0]);
-                } else {
-                    println!("Odd {num} {n_digits}")
+                    println!("VALID {num}")
                 }
             }
         } else {
